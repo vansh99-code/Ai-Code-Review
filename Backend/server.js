e1ff5cd5339d2b92c6d72273e88f18f81109e0ea
vansh99-code <bansalvansh253@gmail.com>
2025-03-29 11:16:51 +0530

    Adding port

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const app = require('./src/app')
 
-app.listen(3000, () => {
+
+app.listen("https://ai-code-review-flame.vercel.app/" || 3000, () => {
     console.log('Server is running...')
 })
   
